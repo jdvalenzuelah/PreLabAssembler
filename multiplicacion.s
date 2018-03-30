b12d8a702a391db34291fd1ec17825ba98ce3cbe
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-03-30 15:46:33 -0600

    multiplicacion

--- multiplicacion.s
+++ multiplicacion.s
@@ -1,5 +1,5 @@
 /* Programa hello.word */
-/* Autor: David Valenzuela */
+/* Autor: David Valenzuela 	 */
 
 .text
 .align 2
@@ -8,5 +8,8 @@
 
 main:
 	stmfd sp!, {lr}	/* SP = R13 link register */
-	MUL R2, #3, #4
+	mov R2, #0
+	mov R3, #3
+	mov R4, #4
+	MUL R2, R3, R4
 	bl puts
