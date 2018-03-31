48dd305b6777a129ece1e1f8e96ce190b379789e
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-03-31 15:13:41 -0600

    d

--- multiplicacion.s
+++ multiplicacion.s
@@ -9,11 +9,13 @@ num2: .word 5
 main:
         push {ip, lr}
 
-        ldr r0, =string
+       /* ldr r0, =string
         ldr r5, adrn1
         ldr r6, adrn2
         str r1, [r5]
-        str r2, [r6]
+        str r2, [r6]*/
+        mov r1, #3
+        mov r2, #3
         mul r5, r1, r2
         bl printf
 
