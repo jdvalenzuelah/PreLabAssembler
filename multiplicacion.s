1cb9ebc2a7de13ce7b18e922781b639c4b79e3f0
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-03-31 15:08:33 -0600

    d

--- multiplicacion.s
+++ multiplicacion.s
@@ -1,3 +1,8 @@
+.data
+string: .asciz "El resultado es: %d\n"
+num1: .word 9
+num2: .word 5
+
 .text
 .global main
 .extern printf
@@ -5,11 +10,13 @@ main:
         push {ip, lr}
 
         ldr r0, =string
-        mov r1, #1024
+        ldr r5, adrn1
+        ldr r6, adrn2
+        mul r5, r5, r6
         bl printf
 
         pop {ip, pc}
 
-.data
-string: .asciz "The number is: %d\n"
+adrn1: .word num1
+adrn2: .word num2
 
