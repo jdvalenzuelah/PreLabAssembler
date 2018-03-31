37dbcadcc11078b36cb3f5ec5c91f63ee4a367dd
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-03-31 15:40:29 -0600

    s

--- multiplicacion.s
+++ multiplicacion.s
@@ -4,25 +4,28 @@
 @Datos
 .data
 string: .asciz "El resultado es: %d\n" @String a imprimir
-num1: .word 10
-num2: .word 5
+num1: .word 10 @Primer numero
+num2: .word 5 @Segundo nimero
 
 .text
 .global main
 .extern printf
 main:
-        push {ip, lr}
+        push {ip, lr} @ip y lr son añadidos al stack
 
-        ldr r0, =string
+        /*cargamos los datos*/
+        ldr r0, =string 
         ldr r5, adrn1
         ldr r6, adrn2
         ldr r1, [r5]
         ldr r2, [r6]
+        /* Ejecutamos la instruccion*/
         mul r1, r1, r2
-        bl printf
+        bl printf @Imprimimos resultado
 
-        pop {ip, pc}
-
+        pop {ip, pc} @pop del ip y pc al stack
+        
+/* direcciones de los numeros en memoria*/
 adrn1: .word num1
 adrn2: .word num2
 
